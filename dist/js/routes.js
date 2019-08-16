a62d2a9a12e75a764f0f46647327c4ad76ac8cce
Roy Tang <roytang.rb@gmail.com>
2019-08-16 17:47:57 +0800

    map line template and data structure

--- dist/js/routes.js
+++ dist/js/routes.js
@@ -1,3 +1,5 @@
+//https://echarts.apache.org/examples/zh/editor.html?c=geo-lines
+
 const initChart = id => {
   const chart = echarts.init(document.getElementById(id))
   chart.showLoading()
@@ -34,22 +36,26 @@ function renderChinaRoutes(){
 	        }
 	    },
 	    series: [{
-	        type: 'linesGL',
-
-	        coordinateSystem: 'geo',
-
+	        type: 'lines',
 	        blendMode: 'lighter',
-
-	        dimensions: ['value'],
-
-	        data: new Float64Array(),
-	        polyline: true,
-	        large: true,
-
 	        lineStyle: {
 	            color: 'orange',
 	            opacity: 0.3
-	        }
+	        },
+	        symbol: ['none', 'arrow'],
+	        symbolSize: 10,
+	        effect: {
+	            show: true,
+	            period: 6,
+	            trailLength: 0,
+	            symbol: 'path://M1705.06,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705.06,1318.313z',
+	            symbolSize: 15
+	        },
+	        data: [{
+	        	fromName: 'random1',
+	        	toName: 'random2',
+	        	coords: [[103.81907349, 36.5617654559],[100.4454754, 21.95735901]],	
+	        }],
 	    }]
 	}
   chinaRoutes.setOption(option);
