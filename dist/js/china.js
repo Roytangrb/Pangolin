0fac46b7bb02b958acce3a83a23e50f56c4304c5
Roy Tang <roytang.rb@gmail.com>
2019-08-19 20:16:44 +0800

    change consumption map to choropleth

--- dist/js/china.js
+++ dist/js/china.js
@@ -7,7 +7,7 @@ const initChart = id => {
 var map1 = initChart('choropleth-map1')
 var map2 = initChart('choropleth-map2')
 var hosScatter = initChart('scatter')
-var courtScatter = initChart('scatter-court')
+var consumption = initChart('seizure-consumption')
 var courtAllScatter = initChart('scatter-court-all')
 var seizureLoc = initChart('seizure-locations')
 var seizureLocByParts = initChart('seizure-locations-by-part')
@@ -21,7 +21,7 @@ fetch('assets/chinageo.json')
       render1()
       render2()
       fetchScatterData()
-      fetchCourtScatterData()
+      renderConsumption()
       fetchCourtScatterAllData()
       renderSeizureLoc()
       renderSeizureLocByParts()
@@ -34,14 +34,6 @@ function fetchScatterData(){
       .then(res => res.json())
       .then(data => {
         renderHosScatter(data)
-      })
-}
-
-function fetchCourtScatterData(){
-    fetch('assets/lnglat_court_itslaw.json')
-      .then(res => res.json())
-      .then(data => {
-        renderCourtScatter(data)
       })
 }
 
@@ -269,9 +261,9 @@ function renderHosScatter (data) {
     hosScatter.setOption(option);
 };
 
-function renderCourtScatter (data) {
+function renderConsumption () {
     //https://echarts.baidu.com/blog/2016/04/28/echarts-map-tutorial.html
-    courtScatter.hideLoading();
+    consumption.hideLoading();
   
     option = {
         title: {
@@ -291,29 +283,62 @@ function renderCourtScatter (data) {
         toolbox: {
             show: false,
         },
-        geo: {
-            map: 'China',
-            itemStyle: {          // 定义样式
-                normal: {         // 普通状态下的样式
-                    areaColor: '#323c48',
-                    borderColor: '#111'
-                },
-                emphasis: {         // 高亮状态下的样式
-                    areaColor: '#2a333d'
-                }
-            },
-        },
-
-        series: [{
-            name: 'court',
-            type: 'scatter',
-            coordinateSystem: 'geo',
-            symbolSize: 5,
-            data: data
-        }]
+        visualMap: {
+          left: 'right',
+          min: 0,
+          max: 32,
+          inRange: {
+              color: ['#ffffbf', '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026']
+          },
+          text:['Max','Min'],           // 文本，默认为数值文本
+          calculable: true
+      },
+      series: [
+          {
+              name: '',
+              type: 'map',
+              map: 'China',
+              itemStyle:{
+                  emphasis:{label:{show:true}}
+              },
+              data:[
+                  {'name': '内蒙古', 'value': 0},
+                  {'name': '北京', 'value': 0},
+                  {'name': '吉林', 'value': 0},
+                  {'name': '四川', 'value': 0},
+                  {'name': '天津', 'value': 0},
+                  {'name': '宁夏', 'value': 0},
+                  {'name': '安徽', 'value': 0},
+                  {'name': '山东', 'value': 0},
+                  {'name': '山西', 'value': 0},
+                  {'name': '新疆', 'value': 0},
+                  {'name': '河北', 'value': 0},
+                  {'name': '湖北', 'value': 0},
+                  {'name': '甘肃', 'value': 0},
+                  {'name': '西藏', 'value': 0},
+                  {'name': '贵州', 'value': 0},
+                  {'name': '辽宁', 'value': 0},
+                  {'name': '陕西', 'value': 0},
+                  {'name': '青海', 'value': 0},
+                  {'name': '黑龙江', 'value': 0},
+                  {'name': '上海', 'value': 1},
+                  {'name': '云南', 'value': 9},
+                  {'name': '广东', 'value': 19},
+                  {'name': '广西', 'value': 6},
+                  {'name': '江苏', 'value': 2},
+                  {'name': '江西', 'value': 1},
+                  {'name': '河南', 'value': 1},
+                  {'name': '浙江', 'value': 32},
+                  {'name': '海南', 'value': 1},
+                  {'name': '湖南', 'value': 12},
+                  {'name': '福建', 'value': 6},
+                  {'name': '重庆', 'value': 1}
+              ]
+          }
+      ]
     };
   
-    courtScatter.setOption(option);
+    consumption.setOption(option);
 };
 
 function renderCourtAllScatter (data) {
